dcdf59f0510954cd9c483c081c0463f9e7fb2b4b
lor <lorraine.manalo.iics@ust.edu.ph>
2021-02-10 21:09:06 +0800

    fixed ui and login frm

---
+++ Forms/Login.vb
@@ -0,0 +1,14 @@
+﻿Public Class frmLogin
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        Dim mainScreen As New frmMain
+
+        If txtUsername.Text <> "1" Then
+            MessageBox.Show("Incorrect username.", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        ElseIf txtPassword.Text <> "1" Then
+            MessageBox.Show("Incorrect password.", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        Else
+            mainScreen.Show()
+            Close()
+        End If
+    End Sub
+End Class
